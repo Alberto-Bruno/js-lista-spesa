8f46b38971195df0f05e7399677e7948aa354451
Alberto-Bruno <brunoalberto97@outlook.it>
2022-12-14 22:56:27 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log('JS OK');
+
+/*
+- Lista della spesa, 
+stampare in pagina gli elementi della lista individualmente 
+con un ciclo while.
+*/
+
+// Prendo Elementi
+const resultElement = document.getElementById('rsult');
+
+// Lista spesa
+const shoppingList = [
+   'farina',
+   'uova',
+   'burro',
+   'latte'
+];
+
+let myList = '<ul>';
+
+// Valorizzo gli elemeti della spesa
+
